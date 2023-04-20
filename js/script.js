de6c08141ef7c68f84a4177c7dcd0081adc35dbb
alisq <ali.shamas.qadeer@gmail.com>
2023-04-20 12:09:44 -0400

    added files.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+     if ( msLeft <= 1000 ) {
+            element.innerHTML = "Time's up!";
+            document.body.classList.add("timesUp");
+            var audio = new Audio('alarm.wav');
+            audio.play();
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown( "timer", 0, 10 );
